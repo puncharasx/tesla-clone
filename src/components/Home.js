e47f276307a6e2becb3e2c36b0449798815f9022
Puncharas Phosap <lucifer2002x@gmail.com>
2021-05-17 08:46:12 +0700

    hahaahah

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,30 @@ import styled from 'styled-components'
 import Section from './Section'
 
 const Home = () => {
+    const segments = [
+        'better luck next time',
+        'won 70',
+        'won 10',
+        'better luck next time',
+        'won 2',
+        'won uber pass',
+        'better luck next time',
+        'won a voucher'
+      ]
+      const segColors = [
+        '#EE4040',
+        '#F0CF50',
+        '#815CD1',
+        '#3DA5E0',
+        '#34A24F',
+        '#F9AA1F',
+        '#EC3F3F',
+        '#FF9000'
+      ]
+      const onFinished = (winner) => {
+        console.log(winner)
+      }
+      
     return (
     <Container>
         <Section
@@ -55,6 +79,7 @@ const Home = () => {
             
         />
     </Container>
+
     )
 }
 
